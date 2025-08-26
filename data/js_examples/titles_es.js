0da15988d9143f0ef4e91d5a0c3864e3489372c3
Jose Luis Quiroga <joseluisquiroga@yahoo.com>
2025-08-26 18:59:20 -0500

    c44. writing ex. titles

--- data/js_examples/titles_es.js
+++ data/js_examples/titles_es.js
@@ -1,43 +1,112 @@
 
 
 export const biblang_titles_es = [
-	`Busca versiculos con el código Strong G2288 en la Bizantina (BYZ)`,
-	`Busca versiculos con el código Strong G166 en la Bizantina (BYZ)`,
-	`Busca versiculos con el código Strong H5769 en el Codex de Leningrado Westminister (WLC)`,
-	`Busca versiculos con el código Strong H4194 en el Codex de Leningrado Westminister (WLC)`,
+	`Busca versiculos con el código Strong G2288 en la Bizantina (BYZ) y los muestra en la traducción de la Reina-Valera 1909 (RVA)`,
+	
+	`Busca versiculos con el código Strong G166 en la Bizantina (BYZ) y los muestra en la traducción de la Reina-Valera 1909 (RVA)`,
+	
+	`Busca versiculos con el código Strong H5769 en el Codex de Leningrado Westminister (WLC) y los muestra en la traducción de la Reina-Valera 1909 (RVA)`,
+	
+	`Busca versiculos con el código Strong H4194 en el Codex de Leningrado Westminister (WLC) y los muestra en la traducción de la Reina-Valera 1909 (RVA)`,
+	
 	`Busca versiculos con el texto 'sepul' en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca versiculos con el texto 'subir' en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca versiculos con el texto 'subir' y el texto 'sepul' en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "1_samuel.2" en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "1_samuel.2:6" en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "salmos.30:2-4" en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "ezequiel.37:12" en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca versiculos con el texto 'muer' y el texto 'despe' en la Reina-Valera 1909 (RVA)`,
-	`Busca versiculos con el código Strong H2421 y el código Strong H4191 el Codex de Leningrado Westminister (WLC)`,
+	
+	`Busca versiculos con el código Strong H2421 y el código Strong H4191 en el Codex de Leningrado Westminister (WLC), y los muestra en la traducción de la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "deuteronomio.32:39 ; 1_samuel.2:6 ; salmos.118:17 ; isaías.26:19 ; ezequiel.33:11" en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "1_samuel.2:6" en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "ezequiel.37:12 ; 1_samuel.2:6" en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "ezequiel.37:12 ; 1_samuel.2:6 ; isaías.26:19" en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "mateo.1" sin los versiculos de la cita biblica "mateo.1:3-9" en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "mateo.1" sin los versiculos de la cita biblica "mateo.1:15-20" en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "mateo.1:1-5" sin los versiculos de la cita biblica "mateo.1:3" en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "mateo.1:1-5" sin los versiculos de la cita biblica "mateo.1:2" en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "mateo.1:1-5" sin los versiculos de la cita biblica "mateo.1:4" en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "mateo.1:1-5" que tienen el texto 'Jacob' en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "mateo.1:1-5" que no tienen el texto 'Jacob' en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "mateo.1:1-5" que tienen el texto 'Aram' en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "mateo.1:1-5" que no tienen el texto 'Aram' en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "mateo.1:1-5" en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "mateo.1:1-5" que tienen la Expresion Regular Javascript /B..z/ en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "mateo.1:1-5" que tienen la Expresion Regular Javascript /ss/ en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "mateo.1:1-5" que tienen la Expresion Regular Javascript /(\s+)J.s/ en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "mateo.1:1-5" que tienen la Expresion Regular Javascript /a.i[^\s]/ en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "mateo.1:1-5" que tienen la Expresion Regular Javascript /:$/ en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "mateo.1:1-5" que tienen la Expresion Regular Javascript /^Y/ en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "mateo.1:1-5" que tienen la Expresion Regular Javascript /(\s+)á(\s+)/ en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "mateo.1:1-5" que tienen la Expresion Regular Javascript /(\s+).(\s+)/ en la Reina-Valera 1909 (RVA)`,
+	
 	`Busca los versiculos de la cita biblica "mateo.1:1-5" que tienen la Expresion Regular Javascript /(\s+)a(\s+)/ en la Reina-Valera 1909 (RVA)`,
-	`Cambia la conf. para buscar códigos Strong del Antiguo Testamento (OT) en la Septuaginta (LXX) y luego busca los versiculos con el código Strong G166 en la LXX`,
-	`Cambia la conf. para buscar códigos Strong del Antiguo Testamento (OT) en el Codex de Leningrado Westminister (WLC) y luego busca los versiculos con el código Strong H2421 y el código Strong H4191 en el WLC`,
+	
+	`Cambia la conf. para buscar códigos Strong del Antiguo Testamento (OT) en la Septuaginta (LXX) ; luego busca los versiculos con el código Strong G166 en la LXX ; y finalmente los muestra en la traducción de la Reina-Valera 1909 (RVA)`,
+	
+	`Cambia la conf. para buscar códigos Strong del Antiguo Testamento (OT) en el Codex de Leningrado Westminister (WLC) ; luego busca los versiculos con el código Strong H2421 y el código Strong H4191 en el WLC ; y finalmente los muestra en la traducción de la Reina-Valera 1909 (RVA)`,
+	
 	`Cambia la conf. para buscar códigos Strong del Antiguo Testamento (OT) en la Septuaginta (LXX) y luego muestra los versiculos de la última busqueda`,
+	
+	`Cambia la conf. para buscar códigos Strong del Antiguo Testamento (OT) en el Codex de Leningrado Westminister (WLC) ; luego encuentra los que tienen el cod. Strong H2421 y el cod. H4191 en la WLC ; luego cambia a la LXX y busca el cód. Strong G599 en la LXX ; luego calcula la intersección de ambos conjuntos, es decir los versiculos que estan en ambos conjuntos ; y finalmente muestra los versiculos resultado en la traducción de la RVA`,
+	
+	`Cambia la conf. para buscar códigos Strong del Antiguo Testamento (OT) en el Codex de Leningrado Westminister (WLC) ; luego encuentra los que tienen el cod. Strong H2421 y el cod. H4191 en la WLC ; luego cambia a la LXX y busca el cód. Strong G599 en la LXX ; luego calcula la diferencia de ambos conjuntos, es decir los versiculos que estan en el primer conjunto pero no en el segundo ; y finalmente muestra los versiculos resultado en la traducción de la RVA`,
+	
+	`Cambia la conf. para buscar códigos Strong del Nuevo Testamento (NT) en el texto critico Wescott and Hort (WH) ; luego encuentra la cita biblica "mateo.1:1-5" ; y finalmente muestra los versiculos resultado en la traducción de la RVA. La diferencia con buscar esta misma cita en otro texto NT está en que el analisis textual que muestra la aplicación para los versiculos resultado es diferente para cada conf. de NT, cada analisis corresponde al texto escojido en NT. En este caso el analisis textual para cada versiculo resultado corresponde a el WH`,
+	
+	`Cambia la conf. para buscar códigos Strong del Nuevo Testamento (NT) en la Bizantina (BYZ) ; luego encuentra la cita biblica "mateo.1:1-5" ; y finalmente muestra los versiculos resultado en la traducción de la RVA. La diferencia con buscar esta misma cita en otro texto NT está en que el analisis textual que muestra la aplicación para los versiculos resultado es diferente para cada conf. de NT, cada analisis corresponde al texto escojido en NT. En este caso el analisis textual para cada versiculo resultado corresponde a la BYZ`,
+	
+	`Cambia la conf. para buscar códigos Strong del Nuevo Testamento (NT) en la Bizantina (BYZ) ; luego encuentra los que tienen el cod. Strong G1161 en la BYZ ; luego cambia a el WH y busca el cód. Strong G1161 en el WH ; luego calcula la diferencia de ambos conjuntos, es decir los versiculos que estan en el primer conjunto pero no en el segundo ; y finalmente muestra los versiculos resultado en la traducción de la RVA`,
+	
+	`Cambia la conf. del texto traducido (LOC) a la RVA ; luego muestra los versiculos que tienen el texto 'muert' y el texto 'etern' en la RVA`,
+	
+	`Cambia la conf. del texto traducido (LOC) a la RVA ; luego encuentra los versiculos que tienen el texto 'muert' y el texto 'etern' en la RVA ; luego cambia a la KJV y encuentra los versiculos que tienen el texto 'death' ; luego calcula la diferencia de ambos conjuntos ; y finalmente muestra los versiculos resultado en la traducción de la KJV`,
+	
+	`Busca H7585 en WLC y los muestra RVA`,
+	
+	`Cambia la conf. para buscar texto en la conf. del campo OT (en este caso la WLC) ; luego busca el texto trans-deletreado "wevl" y muestra los versiculos de la WLC trans-deletreados a ASCII`,
+	
+	`Cambia la conf. para buscar texto en la conf. del campo OT (en este caso la WLC) ; luego busca el texto trans-deletreado "wevl" y muestra los versiculos de la WLC con letras hebreas. Para el hebreo tanto 'MIN' como 'MAY' muestran las mismas letras hebreas`,
+	
+	`Busca "lucas.16" en RVA`,
+	
+	`Busca G86 en WH y los muestra en RVA`,
+	
+	`Cambia la conf. para buscar texto en la conf. del campo NT (en este caso la WH) ; luego busca el texto trans-deletreado "adhs" y muestra los versiculos de WH trans-deletreados a ASCII.`,
+	
+	`Cambia la conf. para buscar texto en la conf. del campo NT (en este caso la WH) ; luego busca el texto trans-deletreado "adhs" y muestra los versiculos de WH  con letras griegas minusculas.`,
+	
+	`Cambia la conf. para buscar texto en la conf. del campo NT (en este caso la WH) ; luego busca el texto trans-deletreado "adhs" y muestra los versiculos de WH  con letras griegas mayusculas.`,
+	
 ];
